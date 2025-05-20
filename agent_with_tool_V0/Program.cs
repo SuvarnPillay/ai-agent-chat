de178eddc84835825bcf8668d185b6667c6a5f0d
suvarn <suvarnpillay@gmail.com>
2025-05-20 17:26:11 +0200

    changed log types

--- agent_with_tool_V0/Program.cs
+++ agent_with_tool_V0/Program.cs
@@ -38,11 +38,13 @@ builder.Services.AddSingleton<agent_with_tool_V0.services.PrivateAgent>(sp =>
     var connectionString = config["AzureAIStudio:ConnectionString"];
     var agentId = config["AzureAIStudio:AgentId"];
     var threadId = config["AzureAIStudio:ThreadId"];
+    // Log to console for Azure log stream
+    var logger = sp.GetRequiredService<ILoggerFactory>().CreateLogger("Startup");
+    logger.LogInformation("Connection String: {ConnectionString}", connectionString);
+    logger.LogInformation("Agent ID: {AgentId}", agentId);
+    logger.LogInformation("Thread ID: {ThreadId}", threadId);
     return new agent_with_tool_V0.services.PrivateAgent(connectionString, agentId, threadId);
 });
-Console.WriteLine("Connection String: " + builder.Configuration["AzureAIStudio:ConnectionString"]);
-Console.WriteLine("Agent ID: " + builder.Configuration["AzureAIStudio:AgentId"]);   
-Console.WriteLine("Thread ID: " + builder.Configuration["AzureAIStudio:ThreadId"]);
 
 var app = builder.Build();
 
